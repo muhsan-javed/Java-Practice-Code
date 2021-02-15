198ae1d6f62272378edb1ac2b611b4070d3ada16
Muhsanjaved048 <67718185+Muhsanjaved048@users.noreply.github.com>
2021-02-15 12:56:22 +0500

    inital commad

---
+++ src/com/company/MT_89_Collections_Hierarchy.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MT_89_Collections_Hierarchy {
+    public static void main(String[] args) {
+
+//        ArrayList
+//        Set
+//        TreeSet
+
+    }
+}
---
+++ src/com/company/MT_90_How_to_View_Java_Documentation_Correct_Way.java
@@ -0,0 +1,4 @@
+package com.company;
+
+public class MT_90_How_to_View_Java_Documentation_Correct_Way {
+}
