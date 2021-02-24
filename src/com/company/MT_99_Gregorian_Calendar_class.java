07270a708c43bba00f0639e7372c610a05406b9f
Muhsanjaved048 <67718185+Muhsanjaved048@users.noreply.github.com>
2021-02-24 12:55:38 +0500

    inital commad class 102

---
+++ src/com/company/MT_99_Gregorian_Calendar_class.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class MT_99_Gregorian_Calendar_class {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.getTime());
+        System.out.println(c.get(Calendar.DATE));
+        System.out.println(c.get(Calendar.SECOND));
+        System.out.println(c.get(Calendar.HOUR));
+        System.out.println(c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE));
+
+        GregorianCalendar cal = new GregorianCalendar();
+        System.out.println(cal.isLeapYear(2020));
+
+        System.out.println(TimeZone.getAvailableIDs()[0]);
+        System.out.println(TimeZone.getAvailableIDs()[1]);
+        System.out.println(TimeZone.getAvailableIDs()[2]);
+        System.out.println(TimeZone.getAvailableIDs()[3]);
+        System.out.println(TimeZone.getAvailableIDs()[4]);
+        System.out.println(TimeZone.getAvailableIDs()[5]);
+        System.out.println(TimeZone.getAvailableIDs()[92]);
+
+    }
+}
