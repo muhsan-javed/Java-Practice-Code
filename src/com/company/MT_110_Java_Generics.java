8207e1b7f5cfd87563420f2fe62623157b770e9e
Muhsanjaved048 <67718185+Muhsanjaved048@users.noreply.github.com>
2021-03-01 12:31:11 +0500

    inital commad class 110

---
+++ src/com/company/MT_110_Java_Generics.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class MyGenerics <T1, T2>{
+    int val;
+    private T1 t1;
+    private T2 t2;
+
+    public MyGenerics(int val, T1 t1 , T2 t2){
+        this.val = val;
+        this.t1  = t1;
+        this.t2  = t2;
+    }
+
+    public T2 getT2() {
+        return t2;
+    }
+
+    public void setT2(T2 t2) {
+        this.t2 = t2;
+    }
+
+
+
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public T1 getT1() {
+        return t1;
+    }
+
+    public void setT1(T1 t1) {
+        this.t1 = t1;
+    }
+
+
+}
+
+public class MT_110_Java_Generics {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+//        ArrayList<int> arrayList = new ArrayList<>(); // this will produce an error
+//        arrayList.add("str1");
+        arrayList.add(34);
+        arrayList.add(234);
+//        arrayList.add(new Scanner(System.in));
+
+//        int a =(int) arrayList.get(2);
+//        int a = arrayList.get(2);
+
+//        System.out.println(a);
+
+        MyGenerics<String, Integer> g1 = new MyGenerics(23,"My String is my string",456);
+        String str  = g1.getT1();
+        Integer int1 = g1.getT2();
+        System.out.println(str + "  " +int1);
+
+    }
+}
