99b0ea5af1cd141d5fc5dace67ca525d3ef6b96b
Muhsa <muhsanjaved048@outlook.com>
2021-09-28 10:50:33 +0500

    ad Exercise1 new solution

--- src/com/company/MT_06_Exercise1_CBSE_Board_Percentage_Calculator.java
+++ src/com/company/MT_06_Exercise1_CBSE_Board_Percentage_Calculator.java
@@ -25,5 +25,52 @@ public class MT_06_Exercise1_CBSE_Board_Percentage_Calculator {
         System.out.println("percentage : ");
         System.out.println(percentage);
 
+
+       /* //	public class Percentage{
+        // Storing total mark of cbse board in int i.e 100 * 5 = 500
+        int totalMarks = 500;
+
+        // Making object of Scanner for user input
+        Scanner sc = new Scanner(System.in);
+
+        // Taking student marks in double because marks can be in point also for calculating percentage
+        System.out.println("Enter your marks of  ");
+        double firstSubject = sc.nextDouble();
+
+        System.out.println("Enter your marks of subject 2");
+        double secondSubject = sc.nextDouble();
+
+        System.out.println("Enter your marks of subject 3");
+        double thirdSubject = sc.nextDouble();
+
+        System.out.println("Enter your marks of subject 4");
+        double fourthSubject = sc.nextDouble();
+
+        System.out.println("Enter your marks of subject 5");
+        double fifthSubject = sc.nextDouble();
+
+        // Creating logic that if user give input less than 0 aur more than zero than don't perform any tasks
+
+        if(firstSubject < 0 || firstSubject > 100 ||
+                secondSubject < 0 || secondSubject > 100 ||
+                thirdSubject < 0 || thirdSubject > 100 ||
+                fourthSubject < 0 || fourthSubject > 100 ||
+                fifthSubject < 0 || fifthSubject > 100)
+        {
+            System.out.println("Please enter valid marks");
+        }
+        else{
+            // Storing total marks of all subject in double i.e allMarks
+            double allMarks = firstSubject+secondSubject+thirdSubject+
+                    fourthSubject+firstSubject;
+
+            // calculating percentage and Storing it in double variable i.e percentage
+            double percentage = allMarks/totalMarks*100;
+
+            // printing actual percentage after calculating
+            System.out.println("Your total marks is: "+percentage+"%");
+        }*/
+
+
     }
 }
