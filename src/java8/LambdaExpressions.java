f974941bc3782ece6289efdf422bce79e7738a72
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-09-28 10:28:55 +0500

    java 8 Features

---
+++ src/java8/LambdaExpressions.java
@@ -0,0 +1,28 @@
+package java8;
+
+public class LambdaExpressions {
+    public static void main(String[] args) {
+
+        // Lambda expression is kind of function which have the following charatertics
+        // It is anonymous(NameLass) function
+        // It have no return type.
+        // It have no modifiers (Live - public - static & final)
+        //->() {}
+        // WHY LAMBDA
+        // 1. Enable functional programming
+        // 2. Readable adn concise code
+        // 3.Easier-to-use API and libraries
+        // 4. Enable support of parallel processing.
+        /*
+
+        (Int x) -> {}
+
+        public void show(){
+            System.out.println("Show");
+        }
+
+        // Lambda
+        () -> System.out.println("Show");
+        */
+    }
+}
