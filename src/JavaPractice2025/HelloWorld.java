e298c88428da0e4f8b0ed4cf17ae14dfd746a3dc
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-11-11 08:54:44 +0500

    Practice 11-11-24

---
+++ src/JavaPractice2025/HelloWorld.java
@@ -0,0 +1,30 @@
+package JavaPractice2025;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World Create By Muhsan Javed");
+
+    // A Package in java is used to group related classes. Think of it as a folder in a file directory.
+    // we use packages to avoid name conflicts, and to write a better maintainable code.
+        // Packages are divided into two categories>
+        // Built - in packages from the java Api)
+        // User-defined Packages ( Create your own packages)
+        // Syntax:
+        //import package.name.class // import a single class
+        //import package.name.* // import the whole class
+
+
+        // Class
+        // A class is an entity that determines how an object will behave and what the object will contain.
+        // In other words, it os a blueprint or a set of instruction to build a specific type of object.
+        // Contains variables (fields_) methods & Object...
+        // Syntax:
+        // class <Class_Name> {
+        // Field;
+        // method;
+        // }
+
+        // Data Type
+        //
+    }
+}
